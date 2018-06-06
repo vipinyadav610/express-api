a6f5e1268b5a041a3a7435325d7353f51d3799bd
Vipin yadav <vipin.yadav@asticsinc.com>
2018-06-06 15:18:25 +0530

    graphql mongodb integration for todo app

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 import http from "http";
 import app from "./server";
+const port = 3000;
 const server = http.createServer(app);
 let currentApp = app;
-server.listen(3000);
+server.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
 if (module.hot) {
   module.hot.accept("./server", () => {
     server.removeListener("request", currentApp);
--- server/server.js
+++ server/server.js
@@ -1,8 +1,40 @@
 import express from "express";
+import graphqlHTTP from "express-graphql";
+import { buildSchema } from "graphql";
+import mongoose from "mongoose";
+import ToDo from "./mongoose/todo";
+import schema from "./graphql/Schema/Schema";
 const app = express();
-app.get("/api", (req, res) => {
-  res.send({
-    message: "I am a serve route and can also be hot !"
-  });
+
+mongoose.connect("mongodb://localhost:27017/local");
+var db = mongoose.connection;
+db.on("error", () => {
+  console.log("---FAILED to connect to mongoose");
 });
+db.once("open", () => {
+  console.log("+++Connected to mongoose");
+});
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true
+  })
+);
+// app.get("/api", (req, res) => {
+//   // Insert into TodoList Collection
+//   var todoItem = new ToDo({
+//     itemId: 1,
+//     item: "vipin",
+//     completed: false
+//   });
+//   todoItem.save((err, result) => {
+//     if (err) {
+//       console.log("---TodoItem save failed " + err);
+//     }
+//     console.log("+++TodoItem saved successfully " + todoItem.item);
+//   });
+// });
+
 export default app;
