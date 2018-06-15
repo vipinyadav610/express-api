a9c274a234e514864ab701e29e3943c47f4f0ca0
Vipin yadav <vipin.yadav@asticsinc.com>
2018-06-15 12:13:22 +0530

    added folder structure

--- server/server.js
+++ server/server.js
@@ -1,40 +1,10 @@
 import express from "express";
-import graphqlHTTP from "express-graphql";
-import { buildSchema } from "graphql";
-import mongoose from "mongoose";
-import ToDo from "./mongoose/todo";
-import schema from "./graphql/Schema/Schema";
+import dbconnect from "./dbconnect";
+import middleware from "./middleware";
+import { GetUser } from "./controller/user/user.controller";
 const app = express();
-
-mongoose.connect("mongodb://localhost:27017/local");
-var db = mongoose.connection;
-db.on("error", () => {
-  console.log("---FAILED to connect to mongoose");
-});
-db.once("open", () => {
-  console.log("+++Connected to mongoose");
-});
-
-app.use(
-  "/graphql",
-  graphqlHTTP({
-    schema,
-    graphiql: true
-  })
-);
-// app.get("/api", (req, res) => {
-//   // Insert into TodoList Collection
-//   var todoItem = new ToDo({
-//     itemId: 1,
-//     item: "vipin",
-//     completed: false
-//   });
-//   todoItem.save((err, result) => {
-//     if (err) {
-//       console.log("---TodoItem save failed " + err);
-//     }
-//     console.log("+++TodoItem saved successfully " + todoItem.item);
-//   });
-// });
+dbconnect(app);
+middleware(app);
+app.get("/user", GetUser);
 
 export default app;
